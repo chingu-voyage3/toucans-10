d0ed11b9a0cd8a0ce39c654bf057060cc9a0c2e4
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-05 08:18:20 -0500

    Initial commit for MaterializeCSS

---
+++ index.js
@@ -0,0 +1,55 @@
+const cardInfo =
+"<ul><li>Name: Jane Doe</li><li>Profession: Photographer</li><li>Location: Arizona</li><li>Interests: Music, hiking, archery, hanging out with friends, spending time with family</li></ul>";
+
+$(document).ready(function() {
+$(".panel").click(function(e) {
+  $(this).toggleClass("flipped");
+});
+$(".panel").hover(
+  function() {
+    $(".info")
+      .empty()
+      .append(function() {
+        return cardInfo;
+      });
+  },
+  function() {
+    $(".info")
+      .empty()
+      .append(function() {
+        return "Card Info";
+      });
+  }
+);
+$("#nav-icon").click(function() {
+  $(this).toggleClass("open");
+});
+
+// Query body and toggle for later use
+const body = document.querySelector("body"),
+  toggle = document.querySelector(".toggle-drawer");
+// Remember the last state
+const drawerOpen = false;
+
+// Bind the toggle function on MOUSEDOWN.
+// You can use click element too, but you will
+// get rid of the ~300ms delay on touchscreens.
+toggle.addEventListener("mousedown", function(event) {
+  event.preventDefault();
+  ToggleDrawer();
+});
+
+const ToggleDrawer = function() {
+  if (!drawerOpen) {
+    // classList API comes with HTML%, so you might want to use an older way to get it working with the legacy browsers.
+    $(".drawer-container").click(function() {
+      $("#nav-icon").show();
+    });
+    body.classList.toggle("drawer-open");
+    drawerOpen = true;
+  }
+};
+
+//Materialize dropdown trigger
+$(".dropdown-trigger").dropdown({ hover: false });
+});
