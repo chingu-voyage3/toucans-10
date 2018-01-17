e408c700fdb7f16a38488c5e2339a8ba9acc3cf9
holdercp <holdercp@icloud.com>
2018-01-16 22:52:26 -0500

    Embed code pens from RSS feed

--- index.js
+++ index.js
@@ -4,7 +4,60 @@ $(document).ready(function() {
   $(".card").click(function(e) {
     $(this).toggleClass("flipped");
   });
+
+  // Embed pens on the page
+  embedPens();
 });
+
+// Makes calls to CodePen and renders oEmbed pens
+function embedPens() {
+  // RSS call to get editor picks
+  var ajax = $.ajax({
+    url: 'https://codepen.io/picks/feed/',
+    dataType: 'xml'
+  });
+
+  var penURLs = [];
+
+  // Return result when call is done
+  return ajax.done(function(feed) {
+    // Find each pen link in RSS feed
+    $(feed).find("item").each(function () {
+      var linkUrl = $(this).find("link").text();
+      penURLs.push('url='+linkUrl);
+    });
+    // Get the oEmebed iframes for each pen
+    getEmbedablePens(penURLs);
+  });
+}
+
+// Makes a call to get the oEmbed iframes and renders them on the page
+function getEmbedablePens(penURLs) {
+  var penList = [];
+
+  // Get iframes
+  $(penURLs).each(function(i, pURL) {
+    var ajax = $.ajax({
+      url: "https://codepen.io/api/oembed?",
+      data: pURL,
+      crossDomain: true,
+      dataType: "jsonp"
+    });
+
+    // Push the deffered calls to the list
+    penList.push(ajax);
+  });
+
+  // When calls are finished, embed each one to the page
+  $.when.apply($, penList).then(function() {
+    var openingHTML = '<div class="row"><div class="col s12">';
+    var closingHTML = '</div></div>';
+    $(penList).each(function(i) {
+      var pen = penList[i].responseJSON.html;
+      $('#card-container').append(openingHTML + pen + closingHTML);
+    })
+  });
+};
 
 /* TODO: Cleanup below. Old code that may be useful later */
 
