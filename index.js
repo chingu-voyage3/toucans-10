1b1d9abb9d9baa5c45f0c0a5aa6f3c1bd3baed7c
Shay Meredith <shay.e.meredith@gmail.com>
2018-01-23 20:04:52 -0500

    Delete extra code and comments

--- index.js
+++ index.js
@@ -59,9 +59,7 @@ function getEmbedablePens(penURLs) {
   });
 };
 
-
 //Toggles menu display property
-//JS was not working with media query display change, ran out of time for better solution
 let trigger = document.getElementsByClassName("menu-trigger")[0];
 let counter = 0;
 let menu = document.getElementsByClassName("nav-menu")[0];
@@ -81,63 +79,3 @@ function menuTrigger() {
     counter = 0;
   }
 }
-
-
-/* TODO: Cleanup below. Old code that may be useful later */
-
-/*
-const cardInfo =
-  "<ul><li>Name: Jane Doe</li><li>Profession: Photographer</li><li>Location: Arizona</li><li>Interests: Music, hiking, archery, hanging out with friends, spending time with family</li></ul>";
-*/
-
-/*
-    $(".panel").click(function(e) {
-    $(this).toggleClass("flipped");
-  });
-  $(".panel").hover(
-    function() {
-      $(".info")
-        .empty()
-        .append(function() {
-          return cardInfo;
-        });
-    },
-    function() {
-      $(".info")
-        .empty()
-        .append(function() {
-          return "Card Info";
-        });
-    }
-  );
-  $("#nav-icon").click(function() {
-    $(this).toggleClass("open");
-  });
-
-  // Query body and toggle for later use
-  const body = document.querySelector("body"),
-    toggle = document.querySelector(".toggle-drawer");
-  // Remember the last state
-  const drawerOpen = false;
-
-  // Bind the toggle function on MOUSEDOWN.
-  // You can use click element too, but you will
-  // get rid of the ~300ms delay on touchscreens.
-  toggle.addEventListener("mousedown", function(event) {
-    event.preventDefault();
-    ToggleDrawer();
-  });
-
-  const ToggleDrawer = function() {
-    if (!drawerOpen) {
-      // classList API comes with HTML%, so you might want to use an older way to get it working with the legacy browsers.
-      $(".drawer-container").click(function() {
-        $("#nav-icon").show();
-      });
-      body.classList.toggle("drawer-open");
-      drawerOpen = true;
-    }
-  };
-
-});
-*/
